63061d68ee640b47032acaa1b06decc44cd674fd
EDEN <eden9701@gmail.com>
2024-03-06 16:40:03 +0200

    created all basic functions

---
+++ HealthPoints.h
@@ -0,0 +1,40 @@
+//
+// Created by EDEN on 04/03/2024.
+//
+
+#ifndef UNTITLED4_HEALTHPOINTS_H
+#define UNTITLED4_HEALTHPOINTS_H
+
+#endif //UNTITLED4_HEALTHPOINTS_H
+
+class NegHp {};
+
+class HealthPoints{
+
+private:
+    int hp;
+    int maxHP;
+
+public:
+    HealthPoints(int hp = 100, int maxHP = 100);
+
+    HealthPoints(const HealthPoints& healthPoints) = default;
+    HealthPoints& operator=(const HealthPoints& other) = default;
+
+    HealthPoints() = default;
+    ~HealthPoints() = default;
+
+    HealthPoints& operator+=(const int add);
+    HealthPoints& operator+(const int add);
+    HealthPoints& operator-(const int add);
+    HealthPoints& operator-=(const int add);
+
+    friend bool operator==(const HealthPoints& healthPoints1, const HealthPoints& healthPoints2);
+    friend bool operator<(const HealthPoints& healthPoints1, const HealthPoints& healthPoints2);
+
+};
+
+bool operator>(const HealthPoints& healthPoints1, const HealthPoints& healthPoints2);
+bool operator>=(const HealthPoints& healthPoints1, const HealthPoints& healthPoints2);
+bool operator<=(const HealthPoints& healthPoints1, const HealthPoints& healthPoints2);
+bool operator==(const HealthPoints& healthPoints1, const HealthPoints& healthPoints2);
